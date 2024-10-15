fd82d773de067c2953462e752723fa76fd2f3bfc
aviral shukla <21ucs043@lnmiit.ac.in>
2024-10-15 20:12:33 +0530

    Updated readme

--- backend/config/config.go
+++ backend/config/config.go
@@ -14,10 +14,8 @@ var RedisClient *redis.Client
 
 func LoadEnv() {
 	os.Setenv("PORT", "8080")
-	os.Setenv("DATABASE_URL", "postgresql://geodrawdb_owner:LTv4gunpcx1B@ep-dry-cherry-a128tdxu.ap-southeast-1.aws.neon.tech/geodrawdb?sslmode=require")
 	os.Setenv("REDIS_URL", "localhost:6379")
 }
-
 func InitDB() *gorm.DB {
 	dsn := os.Getenv("DATABASE_URL")
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
